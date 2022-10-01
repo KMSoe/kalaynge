72a27107547696cbb943a9abe52977727a7443b0
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2022-10-01 16:22:26 +0630

    added Alphabet model and migration

---
+++ database/migrations/2022_10_01_094939_create_alphabets_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlphabetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alphabets', function (Blueprint $table) {
+            $table->id();
+            $table->string('symbol');
+            $table->string('name');
+            $table->string('sample_word');
+            $table->string('picture');
+            $table->string('audio')->nullable();
+            $table->string('video')->nullable();
+            $table->integer('sorting')->default(0);
+            $table->foreignId('category_id')->references('id')->on('categories');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alphabets');
+    }
+}
