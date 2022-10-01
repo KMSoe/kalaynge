484d16346cd76f2a427d7e7734f262e66c4483d8
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2022-10-01 20:05:54 +0630

    Number model and migrations

---
+++ database/migrations/2022_10_01_133436_create_numbers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNumbersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('numbers', function (Blueprint $table) {
+            $table->id();
+            $table->string('symbol');
+            $table->string('name');
+            $table->string('picture');
+            $table->string('audio')->nullable();
+            $table->string('video')->nullable();
+            $table->integer('sorting')->default(0);
+            $table->foreignId('category_id')->references('id')->on('categories');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('numbers');
+    }
+}
