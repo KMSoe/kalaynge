8bbfa5f4c541ec693916cea62d71a9ea169772bf
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2022-10-10 15:53:35 +0630

    Animal Resouce API linkage

---
+++ app/Http/Controllers/Api/AnimalController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Animal;
+use Illuminate\Http\Request;
+
+class AnimalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $animals = Animal::select('name', 'picture')->paginate(3);
+
+        return $animals;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $item = Animal::findOrFail($id);
+
+        return response()->json([
+            'status' => true,
+            'data' => $item,
+            'previous' => $item->previous ? route('animals.show', ['animal' => $item->previous]) : null,
+            'next' => $item->next ? route('animals.show', ['animal' => $item->next]) : null,
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
