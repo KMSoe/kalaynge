7cae07c904f72b71c3e9d0984a6b4686c7294879
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2022-10-10 15:50:57 +0630

    Animal Data Model and migration

---
+++ app/Models/Animal.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Animal extends Model
+{
+    use HasFactory ,SoftDeletes;
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function getNextAttribute()
+    {
+        return static::where('id', '>', $this->id)->orderBy('id', 'asc')->value('id');
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public  function getPreviousAttribute()
+    {
+        return static::where('id', '<', $this->id)->orderBy('id', 'desc')->value('id');
+    }
+
+    public function getPictureAttribute($picture)
+    {
+        return asset('storage/animals/pictures/' . $picture);
+    }
+}
