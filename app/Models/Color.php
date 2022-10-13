c57f47b2931724d6b2d1b73b87987ce5d86aebaa
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2022-10-13 09:16:44 +0630

    Color Data Design and model

---
+++ app/Models/Color.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Color extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $casts = [
+        'pictures' => 'array'
+    ];
+
+     /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function getNextAttribute(){
+        return static::where('id', '>', $this->id)->orderBy('id','asc')->value('id');
+    }
+ 
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public  function getPreviousAttribute(){
+        return static::where('id', '<', $this->id)->orderBy('id','desc')->value('id');
+    }
+
+    public function getSymbolPictureAttribute($symbol_picture)
+    {
+        return asset('storage/colors/pictures/symbols/' . $symbol_picture);
+    }
+
+    public function getPicturesAttribute($pictures)
+    {
+        $result = array();
+
+        foreach ($pictures as $item) {
+            $result[] = asset('storage/colors/pictures/examples/' . $item);
+        }
+
+        return $result;
+    }
+}
