9009dbc944ce2f5b0a122c5526dcba90883a313c
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2022-10-18 01:34:10 +0630

    Shape Data model and migration

---
+++ app/Models/Shape.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Shape extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $casts = [
+        'pictures' => 'array'
+    ];
+
+    protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
+     /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function getNextAttribute(){
+        return static::where('id', '>', $this->id)->orderBy('id','asc')->value('id');
+    }
+ 
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public  function getPreviousAttribute(){
+        return static::where('id', '<', $this->id)->orderBy('id','desc')->value('id');
+    }
+
+    public function getSymbolPictureAttribute($symbol_picture)
+    {
+        return asset('storage/shapes/pictures/symbols/' . $symbol_picture);
+    }
+
+    public function getPicturesAttribute($pictures)
+    {
+        $pictures = json_decode($pictures);
+        $result = array();
+
+        foreach ($pictures as $item) {
+            $result[] = asset('storage/shapes/pictures/examples/' . $item);
+        }
+
+        return $result;
+    }
+}
