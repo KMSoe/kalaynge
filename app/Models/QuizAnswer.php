2ad83757470f6cbad9d733596f83fffa5a6bffa9
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2022-10-20 22:19:22 +0630

    QuizAnswer Data model and relationsip to QuizQuestiobn

--- app/Models/QuizAnswer.php
+++ app/Models/QuizAnswer.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class QuizQuestion extends Model
+class QuizAnswer extends Model
 {
     use HasFactory, SoftDeletes;
 
-    public function answers()
+    public function question()
     {
-        return $this->hasMany(QuizAnswer::class);
+        return $this->belongsTo(QuizQuestion::class);
     }
 }
---
+++ database/migrations/2022_10_20_154454_create_quiz_answers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuizAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quiz_answers', function (Blueprint $table) {
+            $table->id();
+            $table->string('text')->nullable();
+            $table->string('picture')->nullable();
+            $table->text('description')->nullable();
+            $table->string('audio')->nullable();
+            $table->string('video')->nullable();
+            $table->foreignId('question_id')->references('id')->on('quiz_questions');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quiz_answers');
+    }
+}
