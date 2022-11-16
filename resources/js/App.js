f3820ae7e12a4da7943ce86051824b5c006d0f19
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2022-11-16 14:17:35 +0630

    added welcome screen

--- resources/js/App.js
+++ resources/js/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Route } from "react-router-dom";
-import WelcomeImg from './assets/images/welcome.png'
+import React from "react";
+import AppStore from "/images/app_store.svg";
+import PlayStore from "/images/play_store.svg";
 
 function App() {
     const bgStyles = {
@@ -8,10 +8,31 @@ function App() {
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
         backgroundSize: "cover",
-    }
+    };
     return (
         <div style={{ ...bgStyles, width: "100vw", height: "100vh" }}>
+            <div className="wabsolute bottom-20 insert-x-1/2">
+                <p className="text-blue-400 font-bold pb-8 lg:pb-6 text-center md:text-left fade-in">
+                    Download our app:
+                </p>
 
+                <div className="flex w-full justify-center md:justify-start pb-24 lg:pb-0 fade-in">
+                    <a href="#">
+                        <img
+                            src={AppStore}
+                            alt="App Store"
+                            className="h-12 pr-4 bounce-top-icons"
+                        />
+                    </a>
+                    <a href="#">
+                        <img
+                            src={PlayStore}
+                            alt="Play Store"
+                            className="h-12 bounce-top-icons"
+                        />
+                    </a>
+                </div>
+            </div>
         </div>
     );
 }
